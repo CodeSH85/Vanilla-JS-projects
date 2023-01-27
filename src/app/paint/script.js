7ba3bcf8a06f344591c4f3149d0b1fe7b331ac29
CodeSH85 <codesh85@gmail.com>
2023-01-27 22:30:05 +0800

    update paint board

--- src/app/paint/script.js
+++ src/app/paint/script.js
@@ -1,29 +1,75 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleDraw();
+})
+
 const canvas = document.querySelector('#canvas');
-paintCanvas();
-function paintCanvas() {
-  if (canvas.getContext) {
-    let ctx = canvas.getContext('2d');
+let isMouseDown = false;
+
+canvas.width = 800;
+let brush = 1;
+let brushColor = null;
+
+const ctx = canvas.getContext('2d');
+const brushSize = document.querySelector('#brushSize');
+const colorPicker = document.querySelector('#colorPicker');
+const bgColorPicker = document.querySelector('#bgColorPicker');
+const clearBtn = document.querySelector('#clearCanvas');
+let canvasX;
+let canvasY;
+
+function handleDraw() {
+  if(!canvas) {
+    throw new Error('Canvas not supported!');
+  }
+  canvas.addEventListener('mousedown', (e) => {
+    isMouseDown = true;
     ctx.beginPath();
-    ctx.moveTo(100, 100);
-    ctx.lineTo(200, 100);
-    ctx.closePath();
+    canvasX = e.clientX - canvas.offsetLeft;
+    canvasY = e.clientY - canvas.offsetTop;
+    ctx.moveTo(canvasX, canvasY);
+  });
+
+  canvas.addEventListener('mousemove', (e) => {
+    if (!isMouseDown) {
+      e.preventDefault();
+      return;
+    }
+    canvasX = e.clientX - canvas.offsetLeft;
+    canvasY = e.clientY - canvas.offsetTop;
+    ctx.lineTo(canvasX, canvasY);
+    ctx.strokeStyle = brushColor;
     ctx.stroke();
-  } else {
-    
-  };
-};
+  });
 
-canvas.addEventListener('mousedown', e => {
-  console.log(e);
-  const div = document.createElement('div');
-  div.style.backgroundColor = 'red';
-  div.style.position = 'absolute';
+  canvas.addEventListener('mouseup', (e) => {
+    isMouseDown = false;
+    ctx.closePath();
+  });
+}
 
-  div.style.width = 20 + 'px';
-  div.style.height = 20 + 'px';
+brushSize.addEventListener('input', handleBrushSize);
 
-  div.style.top = e.clientY + 'px';
-  div.style.left = e.clientX + 'px';
-  canvas.appendChild(div);
-  console.log(div.style.position);
-});
+function handleBrushSize(event) {
+  brush = event.target.value;
+  ctx.lineWidth = brush;
+}
+
+colorPicker.addEventListener('input', handleColor);
+
+function handleColor(event) {
+  brushColor = event.target.value;
+  ctx.strokeStyle = brushColor;
+}
+
+bgColorPicker.addEventListener('input', handleBgColor);
+
+function handleBgColor(event) {
+  ctx.fillStyle = event.target.value;
+}
+
+clearBtn.addEventListener('click', (e) => {
+  canvasX = e.clientX - canvas.offsetLeft;
+  canvasY = e.clientY - canvas.offsetTop;
+  ctx.clearRect(0, 0, canvasX, canvasY);
+})
