b8f68d0001929b5df6fe1ec7d04f3a60bcf5c05f
CodeSH85 <codesh85@gmail.com>
2023-02-19 17:13:52 +0800

    calendar: add date to grid

--- src/app/calendar/script.js
+++ src/app/calendar/script.js
@@ -2,18 +2,52 @@ const ROWS_COUNT = 5;
 const COLUMNS_COUNT = 7;
 
 const calenderContainer = document.querySelector('#calenderContainer');
+const headerContainer = document.createElement('div');
 const bodyContainer = document.createElement('div');
-bodyContainer.style = 'width: 100%;';
+bodyContainer.classList.add('body-container');
+calenderContainer.appendChild(headerContainer);
+headerContainer.classList.add('header-container');
 calenderContainer.appendChild(bodyContainer);
 
-for (let i = 0; i < ROWS_COUNT; i++) {
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+for (let n = 0; n < COLUMNS_COUNT; n++) {
+  const headerCell = document.createElement('div');
+  headerCell.classList.add('header');
+  headerContainer.appendChild(headerCell);
+  headerCell.innerHTML = `<span>${dayList[n]}</span>`;
+}
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth()+1;
+
+function getMonthDays(year, month) {
+  const date = new Date(year, month, 0);
+  return date.getDate();
+}
+
+let dateBuffer = getMonthDays(year, month);
+let dayCounter = 1;
+let start = new Date(year, month, 1).getDay();
+
+for (let row = 0; row < ROWS_COUNT; row++) {
   const rowContainer = document.createElement('div');
-  rowContainer.style = `height: 50px; width: 100%; border: 1px solid red; display: flex;`;
+  rowContainer.classList.add('row-container');
   bodyContainer.append(rowContainer);
-  for (let n = 0; n < COLUMNS_COUNT; n++) {
+  for (let col = 0; col < COLUMNS_COUNT; col++) {
     const cell = document.createElement('div');
-    cell.innerHTML = `${n}`;
-    // bodyContainer.childNodes[n].appendChild(cell);
+    cell.classList.add('cell');
+    if (row === 0 && col < start) {
+      cell.innerHTML = ``;
+    } else if (dayCounter <= dateBuffer) {
+      cell.innerHTML = `${dayCounter}`;
+      dayCounter++;
+    } else {
+      cell.innerHTML = ``;
+    }
     rowContainer.appendChild(cell);
   }
 }
+
+console.log(getMonthDays(year, month));
