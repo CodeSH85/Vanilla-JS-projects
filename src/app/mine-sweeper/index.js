b0f216f8171f60c4b5914111f313cfbcd73a9818
CodeSH85 <codesh85@gmail.com>
2023-02-23 00:59:54 +0800

    mines: add mines checking on cell

--- src/app/mine-sweeper/index.js
+++ src/app/mine-sweeper/index.js
@@ -1,9 +1,7 @@
 const BOARD_SIZE = 10;
 let NUMBER_OF_MINES = 2;
 
-// const board = createBoard(10, 10, 10);
 const board = document.querySelector('#board');
-
 
 // Fisher-Yates
 function getRandomNumbers(num) {
@@ -20,6 +18,7 @@ function createBoard(cols, rows, mines) {
     throw new Error('mines out number the cells!');
   }
   let totalCells = cols * rows;
+  let counter = 0
   let minesArr = [];
   while (minesArr.length < mines) {
     let mineIndex = Math.floor(Math.random() * totalCells);
@@ -27,17 +26,21 @@ function createBoard(cols, rows, mines) {
       minesArr.push(mineIndex);
     }
   }
-  for (let i = 0; i < totalCells; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.id = 'cell-' + i;
-    cell.dataset.num = i;
-    if (minesArr.includes(i)) {
-      cell.classList.add('mine');
+  for (let col = 0; col < cols; col++) {
+    for (let row = 0; row < rows; row++) {
+      const cell = document.createElement('div');
+      cell.dataset.col = row;
+      cell.classList.add('cell');
+      cell.dataset.row = col;
+      counter ++;
+      cell.id = 'cell-' + counter;
+      cell.dataset.index = counter;
+      if (minesArr.includes(counter)) {
+        cell.classList.add('mine');
+      }
+      board.appendChild(cell);
     }
-    board.appendChild(cell);
   }
-
 }
 
 createBoard(10, 10, 10);
@@ -46,7 +49,33 @@ const cellArr = document.querySelectorAll('.cell');
 cellArr.forEach( cell => {
   cell.addEventListener('click', e => {
     cell.classList.add('pressed');
-    console.log(e.target.dataset.num);
+    console.log(e.target.dataset.index);
     console.log(e.target);
+    let count = nextToMine(e.target);
+    if(count>0) {
+      console.log(`There are ${count} mines near this cell.`);
+    } else {
+      console.log(`There are no mine near this cell.`);
+    }
   })
 })
+
+function nextToMine(cell) {
+  let row = Number(cell.dataset.row);
+  let col = Number(cell.dataset.col);
+  console.log(row, col);
+  let counter = 0
+  for(let i = row-1; i <= row+1; i++) {
+    for (let n = col-1; n <= col+1; n++) {
+      if(i === row && n === col) {
+        continue;
+      }
+      const targetCell = 
+      document.querySelector(`[data-col='${n}'][data-row='${i}']`);
+      if (targetCell && targetCell.classList.contains('mine')) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
