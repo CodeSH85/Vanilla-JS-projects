bb137e8b01e693175ba410371519c9251615b7a4
CodeSH85 <codesh85@gmail.com>
2023-02-25 21:49:27 +0800

    drag-select: update drag-section position issue

--- src/app/drag-selection/script.js
+++ src/app/drag-selection/script.js
@@ -1,51 +1,57 @@
-// const box = document.createElement('div');
-
 const selectArea = document.querySelector('#selectArea');
-// for (let i = 0; i < 50; i++) {
-//   const box = document.createElement('div');
-//   box.classList.add('box')
-//   box.id = 'box';
-//   selectArea.append(box);
-// }
-selectArea.addEventListener('mousedown', e => {
-  console.log('down');
-  let x = e.clientX - e.target.offsetLeft;
-  let y = e.clientY - e.target.offsetTop;
 
-  const div = document.createElement('div');
-  div.style = `position: absolute; height: 10px; width: 10px; background: red; top: ${y - 5}px; left: ${x - 5}px;`;
-  selectArea.appendChild(div);
+selectArea.addEventListener('mousedown', e => {
+  let startX = e.clientX - e.target.offsetLeft;
+  let startY = e.clientY - e.target.offsetTop;
+  const start = document.createElement('div');
+  start.id = 'start';
+  start.style = `position: absolute; height: 10px; width: 10px; background: red; top: ${startY - 5}px; left: ${startX - 5}px;`;
+  // selectArea.appendChild(start);
   const div2 = document.createElement('div');
-  div2.style = `position: absolute; height: 0px; width: 0px; background-color: rgba(123, 123, 123, 0.5); top: ${y - 5}px; left: ${x - 5}px;`;
+  div2.style =
+    `position: absolute; height: 0px; width: 0px; z-index: 999; pointer-events: none;
+      background-color: rgba(123, 123, 123, 0.5); top: ${startY}px; left: ${startX}px;
+    `;
   selectArea.appendChild(div2);
 
+  selectArea.addEventListener('mousemove', handleMouseMove);
+
   function handleMouseMove(e) {
-    let curX = e.offsetX;
-    let curY = e.offsetY;
-    if (curX < x) {
-      curX = x - curX;
-      div2.style.left = `${x - curX}px`
+    let curX = e.clientX - selectArea.offsetLeft - startX;
+    let curY = e.clientY - selectArea.offsetTop - startY;
+    if (curX < 0) {
+      div2.style.left = `${startX + curX}px`
+      div2.style.width = `${-curX}px`
+    } else {
+      div2.style.left = `${startX}px`
+      div2.style.width = `${curX}px`
     }
-    if (curY < y) {
-      curY = y - curY;
-      div2.style.top = `${y - curY}px`
+    if (curY < 0) {
+      div2.style.top = `${startY + curY}px`
+      div2.style.height = `${-curY}px`
+    } else {
+      div2.style.top = `${startY}px`
+      div2.style.height = `${curY}px`
     }
-    console.log('X: ' + curX);
-    console.log('Y: ' + curY);
+    // console.log('X: ' + curX);
+    // console.log('Y: ' + curY);
     div2.style.width = curX + 'px';
     div2.style.height = curY + 'px';
   }
 
-  selectArea.addEventListener('mousemove', handleMouseMove);
-  
   selectArea.addEventListener('mouseup', e => {
-    const div = document.createElement('div');
-    let x = e.clientX - e.target.offsetLeft;
-    let y = e.clientY - e.target.offsetTop;
-    div.style = `position: absolute; height: 10px; width: 10px; background: blue; top: ${y - 5}px; left: ${x - 5}px;`;
-    selectArea.appendChild(div);
-    selectArea.removeEventListener('mousemove');
-    selectArea.removeChild(div2);
+    if(e.target.id === 'start') {
+      return;
+    }
+    const end = document.createElement('div');
+    let endX = e.clientX - e.target.offsetLeft;
+    let endY = e.clientY - e.target.offsetTop;
+    end.style = `position: absolute; height: 10px; width: 10px; background: blue; top: ${endY - 5}px; left: ${endX - 5}px;`;
+    // selectArea.appendChild(end);
+    // selectArea.removeEventListener('mousemove');
+    if (selectArea.contains(div2)) {
+      selectArea.removeChild(div2);
+    }
     // console.log('up');
   })
 })
