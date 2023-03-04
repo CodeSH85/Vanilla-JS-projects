0510ed88d7d2c98036d9d7021e350d5aca0fcd8a
CodeSH85 <codesh85@gmail.com>
2023-03-04 18:01:23 +0800

    drag select: update selection

--- src/app/drag-selection/script.js
+++ src/app/drag-selection/script.js
@@ -2,20 +2,29 @@ const selectArea = document.querySelector('#selectArea');
 
 let startX;
 let startY;
-let div2 = document.createElement('div');
+let dragRect = document.createElement('div');
+
+// const test = document.querySelector('#target');
+
+for(let i = 0; i < 10; i++) {
+  const item = document.createElement('div');
+  item.textContent = 'item';
+  item.id = 'target';
+  item.classList.add('card');
+  selectArea.appendChild(item);
+}
+
+const items = document.querySelectorAll('#target');
 
 selectArea.addEventListener('mousedown', e => {
   startX = e.clientX - e.target.offsetLeft;
   startY = e.clientY - e.target.offsetTop;
-  const start = document.createElement('div');
-  start.id = 'start';
-  start.style = `position: absolute; height: 10px; width: 10px; background: red; top: ${startY - 5}px; left: ${startX - 5}px;`;
-  div2 = document.createElement('div');
-  div2.style =
+  dragRect = document.createElement('div');
+  dragRect.style =
     `position: absolute; height: 0px; width: 0px; z-index: 999; pointer-events: none;
       background-color: rgba(123, 123, 123, 0.5); top: ${startY}px; left: ${startX}px;
     `;
-  selectArea.appendChild(div2);
+  selectArea.appendChild(dragRect);
 })
 
 selectArea.addEventListener('mousemove', handleMouseMove);
@@ -24,59 +33,45 @@ function handleMouseMove(e) {
   let curX = e.clientX - selectArea.offsetLeft - startX;
   let curY = e.clientY - selectArea.offsetTop - startY;
   if (curX < 0) {
-    div2.style.left = `${startX + curX}px`
-    div2.style.width = `${-curX}px`
+    dragRect.style.left = `${startX + curX}px`
+    dragRect.style.width = `${-curX}px`
   } else {
-    div2.style.left = `${startX}px`
-    div2.style.width = `${curX}px`
+    dragRect.style.left = `${startX}px`
+    dragRect.style.width = `${curX}px`
   }
   if (curY < 0) {
-    div2.style.top = `${startY + curY}px`
-    div2.style.height = `${-curY}px`
+    dragRect.style.top = `${startY + curY}px`
+    dragRect.style.height = `${-curY}px`
   } else {
-    div2.style.top = `${startY}px`
-    div2.style.height = `${curY}px`
+    dragRect.style.top = `${startY}px`
+    dragRect.style.height = `${curY}px`
   }
-  div2.style.width = curX + 'px';
-  div2.style.height = curY + 'px';
+  dragRect.style.width = curX + 'px';
+  dragRect.style.height = curY + 'px';
 }
 
 selectArea.addEventListener('mouseup', e => {
 
-  if(e.target.id === 'start') {
-    return;
-  }
+  const selectionArea = dragRect.getBoundingClientRect();
 
-  // function isInRange(area, target) {
-  //   let selectionArea = area.getBoundingClientRect();
-  //   let targetElement = target.getBoundingClientRect();
+  // loop through all the items and check if their boundaries intersect with the drag rectangle
+  items.forEach(item => {
+    const targetElement = item.getBoundingClientRect();
+    if (
+      selectionArea.left <= targetElement.right &&
+      selectionArea.right >= targetElement.left &&
+      selectionArea.top <= targetElement.bottom &&
+      selectionArea.bottom >= targetElement.top
+    ) {
+      item.classList.add('selected');
+    } else {
+      item.classList.remove('selected');
+    }
+  });
 
-  //   return ((
-  //     selectionArea.left <= targetElement.left &&
-  //     selectionArea.right >= targetElement.right &&
-  //     selectionArea.top <= targetElement.top &&
-  //     selectionArea.bottom >= targetElement.bottom
-  //   ));
-  // }
-
-  // if (isInRange(div2, test)) {
-  //   test.textContent = 'Selected'
-  //   test.classList.add('selected');
-  // } else {
-  //   test.textContent = 'Unselected'
-  //   test.classList.remove('selected');
-  // }
-
-  const end = document.createElement('div');
-  let endX = e.clientX - e.target.offsetLeft;
-  let endY = e.clientY - e.target.offsetTop;
-  end.style = `
-    position: absolute; height: 10px; width: 10px;
-    background: blue; top: ${endY - 5}px; left: ${endX - 5}px;
-  `;
-  // selectArea.appendChild(end);
-  if (selectArea.contains(div2)) {
-    selectArea.removeChild(div2);
+  // remove the drag rectangle from the DOM
+  if (selectArea.contains(dragRect)) {
+    selectArea.removeChild(dragRect);
   }
 })
 
