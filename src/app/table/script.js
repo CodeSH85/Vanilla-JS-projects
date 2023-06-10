a19c6e3178a9ebf581ec47a4af7603c6b3975b11
CodeSH85 <codesh85@gmail.com>
2023-06-10 23:12:33 +0800

    table: add Buffer rows

--- src/app/table/script.js
+++ src/app/table/script.js
@@ -1,33 +1,43 @@
 import table_data from './data.json' assert {type: "json"};
-import cellSelectBox from './selection.js'
+import cellSelectBox from './selection.js';
 
 const HEADER_HEIGHT = 24.5;
 let START_INDEX = 0;
 const ROW_HEIGHT = 25;
 const DISPLAY_ROWS = 30;
-const CONTAINER_HEIGHT = ROW_HEIGHT * 15;
+const BUFFER_ROWS = 10;
 
 const header = document.querySelector('#header');
 const tableBody = document.querySelector('#tableBody');
-cellSelectBox(tableBody);
+const tableWrapper = document.querySelector('#tableWrapper');
+const scrollExpander = document.querySelector('#scrollExpander');
+const table = document.querySelector('table');
+const section = document.querySelector('section');
 
+document.addEventListener('DOMContentLoaded', e => {
+  renderRows(START_INDEX, DISPLAY_ROWS);
+  // cellSelectBox(tableBody);
+})
+
+// header
 const headerTemplate = `
   <tr>
-    <th>ID</th>
+    <th>ID</th> 
     <th>First Name</th>
     <th>Last Name</th>
     <th>Email</th>
-    <th>Job Title</th>
+    <th>gender</th>
   </tr>
 `;
+header.innerHTML = headerTemplate;
 
 function displayData(start, count) {
+  const cache = {};
   return table_data.slice(start, start + count);
 }
 
 function renderRows(start, count) {
   const rowsFragment = document.createDocumentFragment();
-  
   displayData(start, count).forEach((row, i) => {
     const rowElement = document.createElement('tr');
     rowElement.innerHTML = `
@@ -35,7 +45,7 @@ function renderRows(start, count) {
       <td>${row.first_name}</td>
       <td>${row.last_name}</td>
       <td>${row.email}</td>
-      <td>${row.job_title}</td>
+      <td>${row.gender}</td>
     `;
     rowsFragment.appendChild(rowElement);
   })
@@ -43,24 +53,20 @@ function renderRows(start, count) {
   tableBody.appendChild(rowsFragment);
 }
 
-header.innerHTML = headerTemplate;
-renderRows(START_INDEX, DISPLAY_ROWS);
-
-const tableWrapper = document.querySelector('#tableWrapper');
-const container = document.querySelector('#container');
-const table = document.querySelector('table');
-const section = document.querySelector('section');
-
 tableWrapper.addEventListener('scroll', handleScroll);
-table.style.height = `${displayData(START_INDEX, DISPLAY_ROWS).length * ROW_HEIGHT}px`;
-container.style.height = `${table_data.length * ROW_HEIGHT}px`;
+scrollExpander.style.height = `${table_data.length * ROW_HEIGHT}px`;
 
 function handleScroll(e) {
-  const { scrollTop } = e.target;
-  let startNode = Math.floor(scrollTop / ROW_HEIGHT);
-  
-  START_INDEX = startNode;
-  section.style.top = `${startNode * ROW_HEIGHT + HEADER_HEIGHT}px`
-  renderRows(startNode, DISPLAY_ROWS);
-};
+  let scrollTime = '';
+  setTimeout(() => {
+    const { scrollTop } = e.target;
 
+    let startNode = Math.floor(scrollTop / ROW_HEIGHT) - BUFFER_ROWS;
+
+    startNode = Math.max(0, startNode);
+
+    let offsetY = `${startNode * ROW_HEIGHT + HEADER_HEIGHT}px`;
+    section.style.transform = `translate3d(0, ${offsetY}, 0)`
+    renderRows(startNode, DISPLAY_ROWS + BUFFER_ROWS);
+  }, 150);
+};
