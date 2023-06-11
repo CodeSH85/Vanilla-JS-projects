a6485b3b61248e335c83ac34a82b10059c6870ea
CodeSH85 <codesh85@gmail.com>
2023-06-11 15:14:28 +0800

    table: update bottom buffer and even-odd row

--- src/app/table/script.js
+++ src/app/table/script.js
@@ -5,7 +5,7 @@ const HEADER_HEIGHT = 24.5;
 let START_INDEX = 0;
 const ROW_HEIGHT = 25;
 const DISPLAY_ROWS = 30;
-const BUFFER_ROWS = 10;
+const BUFFER_ROWS = 20;
 
 const header = document.querySelector('#header');
 const tableBody = document.querySelector('#tableBody');
@@ -47,6 +47,13 @@ function renderRows(start, count) {
       <td>${row.email}</td>
       <td>${row.gender}</td>
     `;
+
+    if (row.id % 2 === 0) {
+      rowElement.classList.add('is-even-row');
+    } else {
+      rowElement.classList.add('is-odd-row');
+    }
+
     rowsFragment.appendChild(rowElement);
   })
   tableBody.innerHTML = '';
@@ -56,17 +63,28 @@ function renderRows(start, count) {
 tableWrapper.addEventListener('scroll', handleScroll);
 scrollExpander.style.height = `${table_data.length * ROW_HEIGHT}px`;
 
+let scrollTime = false;
+let throttleTimeout;
+
 function handleScroll(e) {
-  let scrollTime = '';
+  if (scrollTime) return;
   setTimeout(() => {
-    const { scrollTop } = e.target;
+    scrollTime = true;
+    clearTimeout(throttleTimeout);
+
+    const { scrollTop, clientHeight, scrollHeight } = e.target;
 
     let startNode = Math.floor(scrollTop / ROW_HEIGHT) - BUFFER_ROWS;
-
+    let endNode = Math.floor((scrollTop + clientHeight) / ROW_HEIGHT) + BUFFER_ROWS;
     startNode = Math.max(0, startNode);
+    endNode = Math.min(endNode, table_data.length);
 
-    let offsetY = `${startNode * ROW_HEIGHT + HEADER_HEIGHT}px`;
-    section.style.transform = `translate3d(0, ${offsetY}, 0)`
-    renderRows(startNode, DISPLAY_ROWS + BUFFER_ROWS);
+    let offsetY = `${ startNode * ROW_HEIGHT + HEADER_HEIGHT }px`;
+    requestAnimationFrame(() => {
+      section.style.transform = `translate3d(0, ${offsetY}, 0)`;
+      renderRows(startNode, endNode - startNode);
+      scrollTime = false;
+    })
   }, 150);
 };
+
