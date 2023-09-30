44faf286ce1d940be42c25efc5d20cd20c5645a8
CodeSH85 <codesh85@gmail.com>
2023-09-30 23:32:29 +0800

    utils: update getStyle, add qs function

--- src/utils/getStyle.js
+++ src/utils/getStyle.js
@@ -1,18 +1,13 @@
-/*
-
-
-*/
-
 function getStyle (element, property, pureNumValue=false) {
+
   if (!(element instanceof Element)) {
     throw new Error('DOM Element expected.');
   }
 
   let style = window.getComputedStyle(element).getPropertyValue(property);
+  if (!property) style = window.getComputedStyle(element);
 
-  if (!pureNumValue) {
-    return style;
-  }
+  if (!pureNumValue) return style;
   const numRegex = /\d+/;
   const result = style.match(numRegex);
 
