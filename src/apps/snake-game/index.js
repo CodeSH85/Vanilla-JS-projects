3d5d91413b21a743f1df3a0ee5f00c0a5a71bb2d
CodeSH85 <codesh85@gmail.com>
2024-03-10 22:57:20 +0800

    snake: add food eating expand body

--- src/apps/snake-game/index.js
+++ src/apps/snake-game/index.js
@@ -1,13 +1,11 @@
 import { getElement } from '../../utils/getElement.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  setMap(ctx);
-  setGame(ctx);
-  window.requestAnimationFrame(animation);
+  setMap();
+  setFood();
 })
 
-const GRID_COLS = 50;
-const GRID_ROWS = 30;
+const GRID_COLS = 50, GRID_ROWS = 30;
 const CELL_SIZE = 15;
 const CANVAS_HEIGHT = GRID_ROWS * CELL_SIZE;
 const CANVAS_WIDTH = GRID_COLS * CELL_SIZE;
@@ -17,92 +15,117 @@ const ctx = canvas.getContext('2d');
 
 let gameStart = false;
 let gameOver = false;
-let step = 10;
 
-function setMap(ctx) {
+function setMap() {
   canvas.width = CANVAS_WIDTH;
   canvas.height = CANVAS_HEIGHT;
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_WIDTH);
 }
 
-function setGame(ctx) {
-  const { row: foodRow, col: foodCol } = Food();
+function Snake() {
+  const color = '#00f2f2';
+  const body = [
+    {
+      x: Math.floor(GRID_COLS / 2),
+      y: Math.floor(GRID_ROWS / 2)
+    }
+  ];
+  return {color, body}
+}
+
+function Food() {
+  const x = Math.floor(Math.random() * GRID_COLS);
+  const y = Math.floor(Math.random() * GRID_ROWS);
+  return {
+    x, y
+  }
+}
+
+let foodX = Food().x;
+let foodY = Food().y;
+function setFood() {
   ctx.fillStyle = '#f20000';
-  ctx.fillRect(foodRow * CELL_SIZE, foodCol * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  ctx.fillRect(foodX * CELL_SIZE, foodY * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+}
+
+function resetFood() {
+  foodX = Food().x;
+  foodY = Food().y;
 }
 
 const snake = Snake();
-let vx = snake.head.col, vy = snake.head.row;
-let mx = 0, my = 0;
+let snakeX = snake.body[0].x, snakeY = snake.body[0].y;
+let moveX = 0, moveY = 0;
 function animation() {
+  setMap();
+  setFood();
   ctx.fillStyle = snake.color;
-  vx += mx;
-  vy += my;
-  let x = (vx) * CELL_SIZE;
-  let y = (vy) * CELL_SIZE;
-  ctx.fillRect(x, y, CELL_SIZE, CELL_SIZE);
-  if (step === 0) {
-    gameOver = true;
+  snakeX += moveX;
+  snakeY += moveY;
+  if (snake.body.length > 1) {
+    for (let i = snake.body.length - 1; i > 0; i--) {
+      snake.body[i] = { ...snake.body[i - 1] };
+    }
+  }
+
+  snake.body[0].x += moveX;
+  snake.body[0].y += moveY;
+
+  if (snake.body[0].x === foodX && snake.body[0].y === foodY) {
+    const newBodyNode = { ...snake.body[snake.body.length - 1] };
+    snake.body.push(newBodyNode);
+    resetFood();
+    console.log('food!');
+    console.log(snake.body);
+  }
+
+  // draw the snake
+  for (const node of snake.body) {
+    ctx.fillRect(
+      node.x * CELL_SIZE,
+      node.y * CELL_SIZE,
+      CELL_SIZE,
+      CELL_SIZE
+    );
   }
   if (!gameOver) {
     setTimeout(() =>
       window.requestAnimationFrame(animation)
-    , 80);
+    , 120);
   }
 }
 
-function Snake(length) {
-  const direction = [0, 1];
-  const color = '#00f2f2';
-  const head = {
-    col: Math.floor(GRID_COLS / 2),
-    row: Math.floor(GRID_ROWS / 2)
-  };
-  const body = [];
-  function turn(x, y) {
-    direction[0] = x
-    direction[1] = y
-  }
-  return {
-    color,
-    head,
-    body,
-    turn,
-    direction
-  }
-}
-
-function Food() {
-  const row = Math.floor(Math.random() * GRID_COLS);
-  const col = Math.floor(Math.random() * GRID_ROWS);
-  return {
-    row, col
-  }
-}
-
-let direction = 'up';
 document.addEventListener('keydown', keyEvent);
 function keyEvent(e) {
   const { code } = e;
   switch (code) {
     case 'ArrowUp':
-      mx = 0;
-      my = -1;
+      if (moveY !== 1) {
+        moveX = 0;
+        moveY = -1;
+      }
       break;
     case 'ArrowDown':
-      mx = 0;
-      my = 1;
+      if (moveY !== -1) {
+        moveX = 0;
+        moveY = 1;
+      }
       break;
     case 'ArrowRight':
-      mx = 1;
-      my = 0;
-      direction = 'right';
+      if (moveX !== -1) {
+        moveX = 1;
+        moveY = 0;
+      }
       break;
     case 'ArrowLeft':
-      mx = -1;
-      my = 0;
+      if (moveX !== 1) {
+        moveX = -1;
+        moveY = 0;
+      }
       break;
+    case 'Enter':
+      window.requestAnimationFrame(animation);
     default:
       break;
   }
