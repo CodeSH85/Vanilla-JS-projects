834bacb120f0170fc927521f751d49247e1d5972
CodeSH85 <codesh85@gmail.com>
2024-05-31 23:29:18 +0800

    table: refactor code structure

--- src/apps/table/script.js
+++ src/apps/table/script.js
@@ -1,89 +1,85 @@
-import table_data from './data.json' assert {type: "json"};
-import cellSelectBox from './selection.js';
+import table_data from './data.json' with {type: "json"};
+import { getEle, debounce, createEle } from '../../utils/helpers.js';
+
+const headers = [
+  {
+    key: 'id',
+    title: 'ID'
+  },
+  {
+    key: 'last_name',
+    title: 'Last Name'
+  },
+  {
+    key: 'email',
+    title: 'Email'
+  }
+]
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderColumns({columns: headers});
+  renderRows({ rows: table_data, columns: headers});
+})
 
 const HEADER_HEIGHT = 24.5;
 let START_INDEX = 0;
+
 const ROW_HEIGHT = 25;
 const DISPLAY_ROWS = 30;
-const BUFFER_ROWS = 20;
+const BUFFER_ROWS = 15;
 
-const header = document.querySelector('#header');
-const tableBody = document.querySelector('#tableBody');
-const tableWrapper = document.querySelector('#tableWrapper');
-const scrollExpander = document.querySelector('#scrollExpander');
-const table = document.querySelector('table');
-const section = document.querySelector('section');
+let scrollTop = 0;
 
-document.addEventListener('DOMContentLoaded', e => {
-  renderRows(START_INDEX, DISPLAY_ROWS);
-  // cellSelectBox(tableBody);
-})
+// DOM
+const tableWrapper = getEle('#tableWrapper');
+const tr = createEle('tr');
 
 // header
-const headerTemplate = `
-  <tr>
-    <th>ID</th> 
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Email</th>
-    <th>gender</th>
-  </tr>
-`;
-header.innerHTML = headerTemplate;
+const headerElement = getEle('#header');
+headerElement.appendChild(tr);
+function renderColumns({ columns }) {
+  for (let i = 0; i < columns.length; i++) {
+    const column = columns[i];
+    const th = createEle('th');
+    th.innerHTML = column.title;
+    headerElement.appendChild(th);
+  }
+}
+
+// body
+const tableBody = getEle('#tableBody');
+function renderRows({rows, columns}) {
+  for (let r = 0; r < rows.length; r++) {
+    const tr = createEle('tr');
+    for (let c = 0; c < columns.length; c++) {
+      const td = createEle('td');
+      td.innerHTML = rows[r][columns[c].key];
+      tr.appendChild(td);
+    }
+    tableBody.appendChild(tr);
+  }
+}
 
 function displayData(start, count) {
-  const cache = {};
   return table_data.slice(start, start + count);
 }
 
-function renderRows(start, count) {
-  const rowsFragment = document.createDocumentFragment();
-  displayData(start, count).forEach((row, i) => {
-    const rowElement = document.createElement('tr');
-    rowElement.innerHTML = `
-      <td>${row.id}</td>
-      <td>${row.first_name}</td>
-      <td>${row.last_name}</td>
-      <td>${row.email}</td>
-      <td>${row.gender}</td>
-    `;
+const displayRowCounts = () => {
+}
 
-    if (row.id % 2 === 0) {
-      rowElement.classList.add('is-even-row');
-    } else {
-      rowElement.classList.add('is-odd-row');
-    }
+function calcRowHeight() {
+  let height = '';
+  return height;
+}
 
-    rowsFragment.appendChild(rowElement);
-  })
-  tableBody.innerHTML = '';
-  tableBody.appendChild(rowsFragment);
+function getScrollExpanderHeight() {
+  return table_data.length * ROW_HEIGHT;
 }
 
 tableWrapper.addEventListener('scroll', handleScroll);
-scrollExpander.style.height = `${table_data.length * ROW_HEIGHT}px`;
 
 let scrollTime = false;
 let throttleTimeout;
 
 function handleScroll(e) {
-  if (scrollTime) return;
-  throttleTimeout = setTimeout(() => {
-    scrollTime = true;
-    clearTimeout(throttleTimeout);
-
-    const { scrollTop, clientHeight, scrollHeight } = e.target;
-
-    let startNode = Math.floor(scrollTop / ROW_HEIGHT) - BUFFER_ROWS;
-    let endNode = Math.floor((scrollTop + clientHeight) / ROW_HEIGHT) + BUFFER_ROWS;
-    startNode = Math.max(0, startNode);
-    endNode = Math.min(endNode, table_data.length);
-
-    let offsetY = `${ startNode * ROW_HEIGHT + HEADER_HEIGHT }px`;
-    requestAnimationFrame(() => {
-      section.style.transform = `translate3d(0, ${offsetY}, 0)`;
-      renderRows(startNode, endNode - startNode);
-      scrollTime = false;
-    })
-  }, 150);
 };
