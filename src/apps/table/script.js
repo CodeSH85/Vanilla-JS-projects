c33b6f8a5a7c2788cc6aae2b9bc082cf5eb90261
CodeSH85 <codesh85@gmail.com>
2024-06-01 22:03:48 +0800

    Table: refactor virtualize method with calc tr top

--- src/apps/table/script.js
+++ src/apps/table/script.js
@@ -14,30 +14,29 @@ const headers = [
     key: 'email',
     title: 'Email'
   }
-]
+];
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderColumns({columns: headers});
-  renderRows({ rows: table_data, columns: headers});
+  calcContainerHeight();
+  renderColumns({ columns: headers });
+  displayRows(startIndex, DISPLAY_ROWS);
+  // renderRows({ rows: table_data, columns: headers});
 })
 
-const HEADER_HEIGHT = 24.5;
-let START_INDEX = 0;
-
-const ROW_HEIGHT = 25;
+const ROW_HEIGHT = 40;
 const DISPLAY_ROWS = 30;
-const BUFFER_ROWS = 15;
-
+let startIndex = 0;
 let scrollTop = 0;
 
 // DOM
+const table = getEle('table');
 const tableWrapper = getEle('#tableWrapper');
-const tr = createEle('tr');
 
 // header
 const headerElement = getEle('#header');
-headerElement.appendChild(tr);
 function renderColumns({ columns }) {
+  const tr = createEle('tr');
+  headerElement.appendChild(tr);
   for (let i = 0; i < columns.length; i++) {
     const column = columns[i];
     const th = createEle('th');
@@ -48,9 +47,17 @@ function renderColumns({ columns }) {
 
 // body
 const tableBody = getEle('#tableBody');
+
+function calcContainerHeight() {
+  table.style.height = (table_data.length * ROW_HEIGHT) + 'px';
+}
+
 function renderRows({rows, columns}) {
+  tableBody.innerHTML = '';
   for (let r = 0; r < rows.length; r++) {
     const tr = createEle('tr');
+    tr.style.transform = `translate(0px, ${(r + startIndex) * ROW_HEIGHT}px)`;
+    tr.style.top = '0px';
     for (let c = 0; c < columns.length; c++) {
       const td = createEle('td');
       td.innerHTML = rows[r][columns[c].key];
@@ -58,28 +65,23 @@ function renderRows({rows, columns}) {
     }
     tableBody.appendChild(tr);
   }
+  const sizer = createEle('div');
+  sizer.style.position = 'absolute';
+  sizer.style.visibility = 'hidden';
+  sizer.style.transform = `translate(0px, ${table_data.length * ROW_HEIGHT}px)`;
+  // tableBody.appendChild(sizer);
 }
 
-function displayData(start, count) {
-  return table_data.slice(start, start + count);
-}
-
-const displayRowCounts = () => {
-}
-
-function calcRowHeight() {
-  let height = '';
-  return height;
-}
-
-function getScrollExpanderHeight() {
-  return table_data.length * ROW_HEIGHT;
+// core
+function displayRows(start, end) {
+  const rows = table_data.slice(start, end);
+  renderRows({ rows, columns: headers });
 }
 
 tableWrapper.addEventListener('scroll', handleScroll);
-
-let scrollTime = false;
-let throttleTimeout;
-
 function handleScroll(e) {
+  scrollTop = e.target.scrollTop;
+  startIndex = Math.floor(scrollTop / ROW_HEIGHT);
+  displayRows(startIndex, startIndex + DISPLAY_ROWS);
 };
+
