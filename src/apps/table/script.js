394844d5f6a145e72abcc50088e6656256cfc407
CodeSH85 <codesh85@gmail.com>
2024-06-02 14:51:43 +0800

    table: fix style issue

--- src/apps/table/script.js
+++ src/apps/table/script.js
@@ -20,11 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
   calcContainerHeight();
   renderColumns({ columns: headers });
   displayRows(startIndex, DISPLAY_ROWS);
-  // renderRows({ rows: table_data, columns: headers});
 })
 
-const ROW_HEIGHT = 40;
 const DISPLAY_ROWS = 30;
+let rowHeight = 24;
 let startIndex = 0;
 let scrollTop = 0;
 
@@ -36,27 +35,27 @@ const tableWrapper = getEle('#tableWrapper');
 const headerElement = getEle('#header');
 function renderColumns({ columns }) {
   const tr = createEle('tr');
-  headerElement.appendChild(tr);
   for (let i = 0; i < columns.length; i++) {
     const column = columns[i];
     const th = createEle('th');
     th.innerHTML = column.title;
-    headerElement.appendChild(th);
+    tr.appendChild(th);
   }
+  headerElement.appendChild(tr);
 }
 
 // body
 const tableBody = getEle('#tableBody');
 
 function calcContainerHeight() {
-  table.style.height = (table_data.length * ROW_HEIGHT) + 'px';
+  table.style.height = (table_data.length * rowHeight + 24) + 'px';
 }
 
 function renderRows({rows, columns}) {
   tableBody.innerHTML = '';
   for (let r = 0; r < rows.length; r++) {
     const tr = createEle('tr');
-    tr.style.transform = `translate(0px, ${(r + startIndex) * ROW_HEIGHT}px)`;
+    tr.style.transform = `translate(0px, ${(r + startIndex) * rowHeight}px)`;
     tr.style.top = '0px';
     for (let c = 0; c < columns.length; c++) {
       const td = createEle('td');
@@ -65,23 +64,17 @@ function renderRows({rows, columns}) {
     }
     tableBody.appendChild(tr);
   }
-  const sizer = createEle('div');
-  sizer.style.position = 'absolute';
-  sizer.style.visibility = 'hidden';
-  sizer.style.transform = `translate(0px, ${table_data.length * ROW_HEIGHT}px)`;
-  // tableBody.appendChild(sizer);
 }
 
 // core
+tableWrapper.addEventListener('scroll', handleScroll);
+function handleScroll(e) {
+  scrollTop = e.target.scrollTop;
+  startIndex = Math.floor(scrollTop / rowHeight);
+  displayRows(startIndex, startIndex + DISPLAY_ROWS);
+};
+
 function displayRows(start, end) {
   const rows = table_data.slice(start, end);
   renderRows({ rows, columns: headers });
 }
-
-tableWrapper.addEventListener('scroll', handleScroll);
-function handleScroll(e) {
-  scrollTop = e.target.scrollTop;
-  startIndex = Math.floor(scrollTop / ROW_HEIGHT);
-  displayRows(startIndex, startIndex + DISPLAY_ROWS);
-};
-
