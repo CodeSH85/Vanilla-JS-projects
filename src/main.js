8bbb6720fab30f6fb6789e5ec797cff8201c6f84
CodeSH85 <codesh85@gmail.com>
2024-06-16 10:11:27 +0800

    chore: add style-loader & css-loader on webpack

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
-import { getEle } from "./utils/helpers.js";
+import route from "./router/index.js";
+import { getEle } from "./utils/dom.js";
+import * as css from "./assets/style.css";
 
-import NavBar from "./components/navbar/navbar.js";
-import Footer from "./components/footer/footer.js";
+// import NavBar from "./components/navbar/navbar.js";
+// import Footer from "./components/footer/footer.js";
 
-customElements.define('nav-bar', NavBar);
-customElements.define('footer-comp', Footer);
+// customElements.define('nav-bar', NavBar);
+// customElements.define('footer-comp', Footer);
 
-const urlList = [
-  {
-    title: 'paint',
-    link: './src/apps/paint/index.html'
-  },
-  {
-    title: 'clock',
-    link: './src/apps/clock/index.html'
-  },
-  {
-    title: 'drag-drop',
-    link: './src/apps/drag-drop-select/index.html'
-  },
-  {
-    title: 'virtual scroll',
-    link: './src/apps/virtual-table/index.html'
-  }
-];
-
-function buildLink({ title, link }) {
-  urlList.push({
-    title,
-    link
-  });
-}
+const app = getEle('#app');
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,20 +19,17 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+    ],
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true
-  },
-  module: {
-    rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //   },
-      // },
-    ],
   }
 };
